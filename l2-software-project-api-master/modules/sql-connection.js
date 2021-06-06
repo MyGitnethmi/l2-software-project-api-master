ecbfaa77cbfdb255d2f14a6d0e16b26d1fa2e8e9
asus <MyGitnethmi>
2021-06-06 23:59:26 +0530

    #1

---
+++ l2-software-project-api-master/modules/sql-connection.js
@@ -0,0 +1,24 @@
+const sql = require('mssql');
+
+const config = {
+    server: 'localhost',
+    user: 'nethmi',
+    password: '123456789',
+    database: 'imsdb',
+    options: {
+        enableArithAbort: true,
+        encrypt: true
+    }
+}
+// mssqlserver.cb9qno8m1wma.ap-south-1.rds.amazonaws.com
+
+const poolPromise = new sql.ConnectionPool(config).connect().then(pool => {
+    console.log('Successfully connected to the MS SQL Server!');
+    return pool;
+}).catch(error => {
+    console.log('Error: ' + error);
+});
+
+module.exports = {
+    sql, poolPromise
+}
